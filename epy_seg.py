6b4e8156158d2ddb254fbd4e57aaf5c25076a086
Noam Dori <TheNODO55@gmail.com>
2022-04-07 11:03:23 +0300

    Added vertex detector to the refined epy-seg stage. We no longer need to use the TA GUI for automatic segmentation

--- epy_seg.py
+++ epy_seg.py
@@ -1,8 +1,34 @@
+import numpy as np
+import scipy.ndimage
+import tifffile
 import yaml
 from epyseg.deeplearning.deepl import EZDeepLearning
 import os
 
 from epy_seg_config import EPySegConfig
+
+
+def find_vertices(input_path):
+    for img_dir in os.listdir(input_path):
+        if not os.path.isdir(input_path + "/" + img_dir):
+            continue
+        # load the segmented image
+        seg_img = tifffile.imread(input_path + "/" + img_dir + "/handCorrection.tif")[:, :]
+        seg_img[seg_img > 0] = 1
+
+        img_size = len(seg_img)
+        mask = np.zeros((img_size, img_size)).astype(np.uint8)
+        for i in range(0, img_size):
+            for j in range(0, img_size):
+                if seg_img[i, j] == 1:
+                    kernel = np.zeros((3, 3))
+                    kernel[i == 0:3 - (i == img_size - 1), j == 0:3 - (j == img_size - 1)] += \
+                        seg_img[max(i - 1, 0):min(i + 1, img_size - 1) + 1, max(j - 1, 0):min(j + 1, img_size - 1) + 1]
+                    labels = scipy.ndimage.measurements.label(np.logical_not(kernel), [[0, 1, 0], [1, 1, 1], [0, 1, 0]])
+                    if np.max(labels[0]) >= 3:
+                        mask[i, j] = 255
+
+        tifffile.imwrite(input_path + "/" + img_dir + "/vertices.tif", mask)
 
 
 def main():
@@ -43,6 +69,9 @@ def main():
 
         deep_ta.predict(predict_generator, output_shape, predict_output_folder=predict_output_folder, batch_size=1,
                         post_process_algorithm=post_process_algorithm, **cfg.misc_args)
+
+        if cfg.refined_mode:
+            find_vertices(input_path)
 
 
 if __name__ == '__main__':
