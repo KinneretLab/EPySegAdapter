890a5d10bbd407aa25d4df05e54b07c04e42b128
Noam Dori <theNODO55@gmail.com>
2024-05-24 21:05:53 +0300

    Updated model of EPy-Seg to include new manual segmentation (2021_05_06_pos6 and SD1_2021_05_06)

--- util_organise_manual_for_training.py
+++ util_organise_manual_for_training.py
@@ -1,21 +1,44 @@
 import os
+import re
 import shutil
 from pathlib import Path
+from typing import List
+
+
+def removesuffix(string: str, suffix_list: List[str]):
+    for suffix in suffix_list:
+        if string.endswith(suffix):
+            string = string[:-len(suffix)]
+    return string
 
 
 def main():
-    epyseg_dir = Path(r"Z:\Analysis\users\Projects\Noam\handCorrectedSegmentation_Jul2023")
+    epyseg_dir = Path(r"Z:\Analysis\users\Noam\TrainingDatasets\epyseg\2021_06_21_pos_2")
+    # use this for using all the images in the folder
+    # frame_list = []
+    # An example for choosing specific frames
+    # frame_list = list(range(1, 327)) + list(range(339, 373)) + list(range(379, 401)) + [657, 662, 663]
+
+    frame_list = []
+
+    # -------------------------
 
     # copy folder/handCorrection.tiff -> out/folder.tiff, folder.tiff -> in/folder.tiff
-    (epyseg_dir / 'in').mkdir()
-    (epyseg_dir / 'out').mkdir()
+    if not (epyseg_dir / 'in').exists():
+        (epyseg_dir / 'in').mkdir()
+    if not (epyseg_dir / 'out').exists():
+        (epyseg_dir / 'out').mkdir()
     for file_path in epyseg_dir.iterdir():
-        if not file_path.is_dir():
-            name_no_extension = file_path.name.removesuffix('.tiff').removesuffix('.tif')
-            shutil.copyfile(file_path,
-                            epyseg_dir / 'in' / (name_no_extension + '.tiff'))
-            shutil.copyfile(epyseg_dir / name_no_extension / 'handCorrection.tif',
-                            epyseg_dir / 'out' / (name_no_extension + '.tiff'))
+        if file_path.is_dir():
+            continue
+        frame = int(re.search('(\\d+)\\.tif', file_path.name).group(1))
+        if len(frame_list) != 0 and frame not in frame_list:
+            continue
+        name_no_extension = removesuffix(file_path.name, ['.tiff', '.tif'])
+        shutil.copyfile(file_path,
+                        epyseg_dir / 'in' / (name_no_extension + '.tiff'))
+        shutil.copyfile(epyseg_dir / name_no_extension / 'handCorrection.tif',
+                        epyseg_dir / 'out' / (name_no_extension + '.tiff'))
 
 
 if __name__ == '__main__':
